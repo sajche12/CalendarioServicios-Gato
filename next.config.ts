e23110323ade300b1bab6c28c0b9c03da9715114
Diego Sajche <sajche98@gmail.com>
2026-07-06 12:22:44 -0600

    feat: instalar y configurar next-pwa, manifest.ts, generar el icono del logotipo y configurar el almacenamiento en caché de tiempo de ejecución de Workbox para consultas de la base de datos Supabase sin conexión

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,37 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  workboxOptions: {
+    disableDevLogs: true,
+    runtimeCaching: [
+      {
+        urlPattern: /^https:\/\/.*\.supabase\.co\/rest\/v1\/.*/,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "supabase-api-cache",
+          expiration: {
+            maxEntries: 100,
+            maxAgeSeconds: 60 * 60 * 24 * 30, // 30 días
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
